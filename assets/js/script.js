836d17c9bef63117fa69afc7073ba2cd330d1239
Ignaz Protagonist <pilibili@protonmail.com>
2022-02-11 01:12:36 -0600

    display without the .00000000000001

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,20 @@
 const X = document.getElementById("x");
 const Y = document.getElementById("y");
 const Z = document.getElementById("z");
-
+var x,y,z;
 
 let acl = new Accelerometer({frequency: 30});
 
 
 acl.addEventListener('reading', () => {
-    X.textContent= acl.x;
-    Y.textContent= acl.y;
-    Z.textContent= acl.z;
+
+    x = Math.round(acl.x *10)/10;
+    y = Math.round(acl.y *10)/10;
+    z = Math.round(acl.z *10)/10;
+
+    X.textContent= x;
+    Y.textContent= y;
+    Z.textContent= z;
   
 });
 
