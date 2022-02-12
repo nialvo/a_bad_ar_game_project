0a4ec219b7da6967c60dcc7a107cf57586cf0007
Ignaz Protagonist <pilibili@protonmail.com>
2022-02-12 03:00:56 -0600

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 
+//character position and rendering
+
 const X = document.getElementById("x");
 const Y = document.getElementById("y");
 const Z = document.getElementById("z");
@@ -20,3 +22,40 @@ acl.addEventListener('reading', () => {
 });
 
 acl.start();
+
+//character rendering
+const gangst = document.getElementById("imageLoader").children;
+let i=0;
+
+function draw(){
+
+    //clear previous drawing
+    ctx.clearRect(0,0,1280,720);
+
+    //draw background
+    ctx.drawImage(gangst[i],pos[i][0], pos[i][1]);
+
+}
+
+
+
+//video feed
+const video = document.querySelector('video');
+
+const constraints = { 
+    audio: false,
+    video: { 
+        width: 1280, 
+        height: 720 
+    } 
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log("nooo"); }); 
