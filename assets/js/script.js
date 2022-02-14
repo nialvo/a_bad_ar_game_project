536700c71dc09cf74e7eef44a3dde6cb55e2a3aa
Ignaz Protagonist <pilibili@protonmail.com>
2022-02-13 22:53:31 -0600

    quaternion test

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,8 @@ let x=0;
 let y=0;
 let z=0;
 
+let pos=[];
+
 
 const options = { frequency: 30, referenceFrame: 'device' };
 const sensor = new AbsoluteOrientationSensor(options);
@@ -39,7 +41,7 @@ const video = document.querySelector('video');
 const canvas = document.getElementById("aug");
 const ctx = canvas.getContext("2d");
 ctx.globalAlpha=1;
-//let gangst = document.getElementById("image1");
+let gangst = document.getElementById("image1");
 
 
 let wid = window.innerWidth;
@@ -53,8 +55,8 @@ video.setAttribute("height",wid);
 canvas.setAttribute("width",wid);
 canvas.setAttribute("height",wid);
 
-//let xInc = w/.3;
-//let incY = Math.round(midY/5);
+let wInc = wid/5;
+
 
 
 const constraints = { 
@@ -76,28 +78,34 @@ const constraints = {
 
 
 
-/* hold on the gangster rendering for now
+
 
 function draw(){
     //clear previous drawing
-    ctx.clearRect(0,0,w,w);
+    ctx.clearRect(0,0,wid,wid);
     //draw 
     ctx.drawImage(gangst,pos[0], pos[1]);  
 }
-*/
+
 
 function loop(){
     // quaternion rotation matrix * original position
     i=I*(1-2*(y*y+z*z))+J*2*(x*y-w*z)+K*2*(w*y+x*z);
     j=I*2*(x*y+w*z)+J*(1-2*(x*x+z*z))+K*2*(y*z-w*x);
     k=I*(x*z-w*y)+J*2*(w*x+y*z)+K*(1-2*(x*x+y*y));
+
+    pos[0]=midX-100+k*wInc;
+    pos[1]=midX-250+j*wInc;
+
+
+
     
     iEl.innerText=Math.round(i*100)/100;
     jEl.innerText=Math.round(j*100)/100;
     kEl.innerText=Math.round(k*100)/100;
 
     
-    //draw();
+    draw();
 }
 
 
